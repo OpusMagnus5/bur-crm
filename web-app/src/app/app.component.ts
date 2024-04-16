7c7e73e8476d6d991b21309a24d064d50d2664c6
OpusMagnus5 <damianbodzioch95@gmail.com>
2024-04-16 17:35:11 +0200

    data binding - two way binding - event binding
    DIRECTIVES

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -1,14 +1,29 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {FormsModule} from '@angular/forms'
+import {NgClass, NgForOf, NgIf, NgStyle} from "@angular/common";
 
-@Component({ //mozna stworzyc za pomoca CLI
+@Component({ //mozna stworzyc za pomoca CLI 'ng generate component nazwa'
   selector: 'app-root', //selektor ktory uzywamy w html, mozemy rowniez uzywac atrybutu lub klasy
   standalone: true, //jesli chcemy standalone
-  imports: [RouterOutlet], //tu dodajemy inne Componenty zamiast @NgModule
+  imports: [RouterOutlet, FormsModule, NgForOf, NgClass, NgStyle, NgIf], //tu dodajemy inne Componenty zamiast @NgModule || FormModule jest potrzebny do two-way binding
   templateUrl: './app.component.html', //tu html
   styleUrl: './app.component.css' //tu css
 })
 export class AppComponent {
   title = 'web-app';
   isAllowed = true;
+  serverNames = ['Testservet', 'Testservet2']
+
+  onCreateClick() {
+    this.isAllowed = false;
+  }
+
+  onPassLetter(event: Event) {
+    console.log((<HTMLInputElement>event.target).value)
+  }
+
+  getColor() {
+    return 'green';
+  }
 }
