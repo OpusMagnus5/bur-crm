fc7f4da4989d574855a5f91043f015e65996bae1
OpusMagnus5 <damianbodzioch95@gmail.com>
2024-04-18 18:37:00 +0200

    prepared localizations

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
 import {HeaderComponent} from "./header/header.component";
 import {AdministrationSideMenuComponent} from './administration/side-menu/administration-side-menu.component';
+import {AdministrationUsersMenuComponent} from "./administration/users/menu/administration-users-menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, AdministrationSideMenuComponent],
+  imports: [HeaderComponent, AdministrationSideMenuComponent, AdministrationUsersMenuComponent],
   templateUrl: './app.component.html', //tu html
   styleUrl: './app.component.css', //tu css
 })
