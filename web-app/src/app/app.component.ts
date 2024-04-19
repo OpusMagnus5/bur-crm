05b974276524603986433c632ef6a84a380059e7
OpusMagnus5 <damianbodzioch95@gmail.com>
2024-04-19 12:18:31 +0200

    switch from bootstrap to angular material

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -1,14 +1,12 @@
-import {Component} from '@angular/core';
-import {HeaderComponent} from "./header/header.component";
-import {AdministrationSideMenuComponent} from './administration/side-menu/administration-side-menu.component';
-import {AdministrationUsersMenuComponent} from "./administration/users/menu/administration-users-menu.component";
+import {Component} from '@angular/core';import { NavigationComponent } from './navigation/navigation.component';
+;
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, AdministrationSideMenuComponent, AdministrationUsersMenuComponent],
-  templateUrl: './app.component.html', //tu html
-  styleUrl: './app.component.css', //tu css
+  imports: [NavigationComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
 }
