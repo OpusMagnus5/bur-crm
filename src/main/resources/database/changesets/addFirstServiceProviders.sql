29863648d5d29d7a815f3000bda55bfd34184ed9
OpusMagnus5 <damianbodzioch95@gmail.com>
2024-05-05 13:59:23 +0200

    BACKEND add first service providers

---
+++ src/main/resources/database/changesets/addFirstServiceProviders.sql
@@ -0,0 +1,8 @@
+INSERT INTO service_provider(spr_uuid, spr_version, spr_bur_id, spr_name, spr_nip, spr_created_at, spr_created_by)
+VALUES (gen_random_uuid(), 0, 137771, 'Opus Magnus Bogna Zaborowska', 9542469063, current_timestamp, 1);
+
+INSERT INTO service_provider(spr_uuid, spr_version, spr_bur_id, spr_name, spr_nip, spr_created_at, spr_created_by)
+VALUES (gen_random_uuid(), 0, 124220, 'BOOKINGANIMALSPA SPÓŁKA Z OGRANICZONĄ ODPOWIEDZIALNOŚCIĄ', 7162826739, current_timestamp, 1);
+
+INSERT INTO service_provider(spr_uuid, spr_version, spr_bur_id, spr_name, spr_nip, spr_created_at, spr_created_by)
+VALUES (gen_random_uuid(), 0, 46039, 'BTLA SKILLS SPÓŁKA Z OGRANICZONĄ ODPOWIEDZIALNOŚCIĄ', 5272872314, current_timestamp, 1);
