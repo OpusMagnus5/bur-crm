7985b76c48dc12f130498d3f2a270b5868cbce6d
OpusMagnus5 <damianbodzioch95@gmail.com>
2024-05-18 14:08:00 +0200

    add coach table

---
+++ src/main/resources/database/changesets/createCoachTable.sql
@@ -0,0 +1,15 @@
+CREATE TABLE coach(
+    coa_id BIGSERIAL PRIMARY KEY,
+    coa_uuid UUID UNIQUE NOT NULL,
+    coa_version INTEGER NOT NULL DEFAULT 0,
+    coa_first_name VARCHAR NOT NULL,
+    coa_last_name VARCHAR NOT NULL,
+    coa_pesel VARCHAR UNIQUE NOT NULL,
+    coa_created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp,
+    coa_modified_at TIMESTAMP WITH TIME ZONE,
+    coa_created_by BIGINT NOT NULL,
+    coa_modified_by BIGINT
+);
+
+CREATE INDEX coach_first_name_idx ON coach(coa_first_name);
+CREATE INDEX coach_last_name_idx ON coach(coa_last_name);
