aba8bcff5b92aa479b7af37f221961b724ef57f5
OpusMagnus5 <damianbodzioch95@gmail.com>
2024-06-24 18:49:23 +0200

    add account details

--- web-app/src/app/account/account-details.component.ts
+++ web-app/src/app/account/account-details.component.ts
@@ -1,12 +1,46 @@
-import {Component} from '@angular/core';
+import {Component, signal, WritableSignal} from '@angular/core';
+import {SubscriptionManager} from "../shared/util/subscription-manager";
+import {GetUseDetailsResponseInterface} from "../user/model/get-use-details-response.interface";
+import {UserHttpService} from "../user/service/user-http.service";
+import {AuthService} from "../auth/auth.service";
+import {LocalizedDatePipe} from "../shared/pipe/localized-date.pipe";
+import {MatChip, MatChipSet} from "@angular/material/chips";
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
+import {MatMenu, MatMenuItem, MatMenuTrigger} from "@angular/material/menu";
+import {TranslateModule} from "@ngx-translate/core";
+import {ShowNoDataDirective} from "../shared/directive/show-no-data.directive";
 
 @Component({
   selector: 'app-account-details',
   standalone: true,
-  imports: [],
+  imports: [
+    LocalizedDatePipe,
+    MatChip,
+    MatChipSet,
+    MatIcon,
+    MatIconButton,
+    MatMenu,
+    MatMenuItem,
+    TranslateModule,
+    MatMenuTrigger,
+    ShowNoDataDirective
+  ],
   templateUrl: './account-details.component.html',
   styles: ['@tailwind base']
 })
 export class AccountDetailsComponent {
 
+  private readonly id: string;
+  private subscriptions: SubscriptionManager = new SubscriptionManager();
+  protected readonly userData: WritableSignal<GetUseDetailsResponseInterface | null> = signal(null);
+
+  constructor(
+    private http: UserHttpService,
+    private auth: AuthService
+
+  ) {
+    this.id = this.auth.getAuthData()!.id;
+    this.http.getUserDetails(this.id).subscribe(response => this.userData.set(response));
+  }
 }
